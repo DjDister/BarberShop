ee7b698bd096a63f535033bdae3ded477e0470e5
Aleksy <psychologianauki@gmail.com>
2023-03-21 20:19:27 +0100

    navbar init

---
+++ pages/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+export default function App() {
+  return (
+    <div style={{ backgroundColor: "black", width: "100%", height: "100%" }}>
+      <Navbar />
+    </div>
+  );
+}
