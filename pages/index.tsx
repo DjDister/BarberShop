b2e73cfbe0434011354b7809ab94cb1c392ce13e
DjFIREED <porebskifilip@wp.pl>
2023-03-22 19:24:12 +0100

    ourservices

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import styles from "@/styles/Home.module.css";
-import App from "./App";
 import { createClient } from "next-sanity";
 import WorkingHours from "@/components/WorkingHours/WorkingHours";
 import Navbar from "@/components/Navbar/Navbar";
+import OurServices from "@/components/OurServices/OurServices";
+import { OurService, WorkingHour } from "@/types";
 
 const client = createClient({
   projectId: "0kjvrvfn",
@@ -13,31 +14,37 @@ const client = createClient({
 
 export default function Home({
   workinghours,
+  ourservices,
 }: {
   workinghours: WorkingHour[];
+  ourservices: OurService[];
 }) {
   return (
-    <div style={{ backgroundColor: "black", width: "100%", height: "100%" }}>
+    <div style={{ width: "100%", height: "100%" }}>
       <Navbar />
       <div style={{ width: "100%", backgroundColor: "red" }}>
         <WorkingHours className={styles.wkcont} workinghours={workinghours} />
+      </div>
+      <div style={{ width: "100%" }}>
+        <div style={{ width: "100%" }}>
+          <OurServices ourservices={ourservices} />
+        </div>
+        <div style={{ width: "50%" }}></div>
       </div>
     </div>
   );
 }
 
-type WorkingHour = {
-  day: string;
-  hours: string;
-};
-
 export async function getStaticProps() {
-  const workinghours = await client.fetch<WorkingHour>(
-    `*[_type == "workinghour"]`
-  );
+  const [workinghours, ourservices] = await Promise.all([
+    client.fetch<WorkingHour>(`*[_type == "workinghour"]`),
+    client.fetch<OurService>(`*[_type == "ourservices"]`),
+  ]);
+
   return {
     props: {
       workinghours,
+      ourservices,
     },
   };
 }
